69fb30647bb369a74b637f1d16daed63fdd955a4
Miguel Avila <mikestandford4@gmail.com>
2024-03-15 21:01:02 -0500

    experimentation done

--- experimentation/setup.sh
+++ experimentation/setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # exit on error
+
 function clean_up() {
-  rm
+  rm "$1" &> /dev/null
 }
 
 function install_dependencies() {
-  cd "$1"
+  cd "$1" || echo "NO SUCH DIRECTORY $1"
   python3 -m venv venv
   source venv/bin/activate
   pip install --upgrade pip
   pip install -r requirements.txt
   deactivate
 }
+
+clean_up "../component-api-gateway/venv"
+clean_up "../component-authentication/venv"
+clean_up "../component-authorization/venv"
 
 install_dependencies "../component-api-gateway"
 install_dependencies "../component-authentication"
